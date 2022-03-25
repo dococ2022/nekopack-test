0709915c720a8a3710e9e8bbe0705223a47dbfcc
ashty-drone <77475908+ashty-drone@users.noreply.github.com>
2022-03-25 12:57:35 +0530

    Update okteto-up.py

--- setup/okteto-up.py
+++ setup/okteto-up.py
@@ -1,15 +1,21 @@
+import os
+from sys import exit
+
+token = os.environ.get("OKTETO_TOKEN")
+if not token: exit("#"*10 + "\nSet OKTETO_TOKEN!!" + "#"*10)
+  
 def okteto_up():
-  import os
   from logging import getLogger
   getLogger("OKTETO").warning("RESTARTING!!")
-  return os.system(string.format(os.getenv("OKTETO_ACCESS_TOKEN")))
+  return exit(os.system(string.format(token)))
 
 string = """\
 okteto context use https://cloud.okteto.com --token {}
 rm -rf nekopack
 git clone https://github.com/ashty-drone/nekopack -b okteto
 cd nekopack
-okteto deploy --build
+okteto deploy
+exit
 """
 
 from datetime import datetime
@@ -18,5 +24,6 @@ start_time = datetime.now()
 while True:
   curr_time = datetime.now()
   uptime = curr_time - start_time
-  
-  if uptime.seconds >= 5: okteto_up()
+  if uptime.seconds >= 120: break
+
+okteto_up()
